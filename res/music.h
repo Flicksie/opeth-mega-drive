21cce7479322aeb4d00b9f005e1b3e10956e50aa
Tsumari <lucasflicky@hotmail.com>
2021-11-26 04:25:31 -0300

    initial commit

---
+++ res/music.h
@@ -0,0 +1,24 @@
+#ifndef _RES_MUSIC_H_
+#define _RES_MUSIC_H_
+
+extern const u8 pcm_hat1[9728];
+extern const u8 pcm_hat2[5632];
+extern const u8 pcm_voice[17408];
+extern const u8 pcm_snare1[9472];
+extern const u8 pcm_snare2[3840];
+extern const u8 xgm_1[31232];
+extern const u8 xgm_2[41472];
+extern const u8 xgm_3[143360];
+extern const u8 xgm_4[51200];
+extern const u8 xgm_5[78336];
+extern const u8 xgm_13[10240];
+extern const u8 xgm_6[104960];
+extern const u8 xgm_7[61952];
+extern const u8 xgm_8[24576];
+extern const u8 xgm_9[39168];
+extern const u8 xgm_10[35584];
+extern const u8 xgm_11[53248];
+extern const u8 xgm_12[74240];
+extern const u8 xgm_14[55552];
+
+#endif // _RES_MUSIC_H_
