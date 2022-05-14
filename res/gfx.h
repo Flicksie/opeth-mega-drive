499f3196483fce00a80fc39adf8038a23a2095be
Tsumari <lucasflicky@hotmail.com>
2022-05-14 11:23:23 -0300

    sega red

--- res/gfx.h
+++ res/gfx.h
@@ -1,6 +1,8 @@
 #ifndef _RES_GFX_H_
 #define _RES_GFX_H_
 
+extern const SpriteDefinition sega_spr;
+extern const SpriteDefinition sega_spr_red;
 extern const SpriteDefinition left_right;
 extern const SpriteDefinition cursor;
 extern const SpriteDefinition shadow_mask_16;
