cb191e4ccfb1eb02946599841393973560715b66
Tsumari <lucasflicky@hotmail.com>
2022-05-14 05:56:18 -0300

    redefine tracks

--- res/music.h
+++ res/music.h
@@ -6,19 +6,10 @@ extern const u8 pcm_hat2[5632];
 extern const u8 pcm_voice[17408];
 extern const u8 pcm_snare1[9472];
 extern const u8 pcm_snare2[3840];
-extern const u8 xgm_1[31232];
-extern const u8 xgm_2[41472];
-extern const u8 xgm_3[143360];
-extern const u8 xgm_4[51200];
-extern const u8 xgm_5[78336];
-extern const u8 xgm_13[10240];
-extern const u8 xgm_6[104960];
-extern const u8 xgm_7[61952];
-extern const u8 xgm_8[24576];
-extern const u8 xgm_9[39168];
-extern const u8 xgm_10[35584];
-extern const u8 xgm_11[53248];
-extern const u8 xgm_12[74240];
-extern const u8 xgm_14[55552];
+extern const u8 xgm_1[212992];
+extern const u8 xgm_2[150272];
+extern const u8 xgm_3[10496];
+extern const u8 xgm_4[346368];
+extern const u8 xgm_5[236288];
 
 #endif // _RES_MUSIC_H_
