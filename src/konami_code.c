05c0be1eccec709116b9fb0a93909373c06138f4
Tsumari <lucasflicky@hotmail.com>
2022-05-18 13:26:14 -0300

    cleanup

--- src/konami_code.c
+++ src/konami_code.c
@@ -1,74 +1,73 @@
-#include "genesis.h"
+#include "types.h"
+#include "joy.h"
 #include "konami_code.h"
 
 
-int konamiSeq = 0;
-int konamiTimer = 60;
-bool konamiTriggered = FALSE;
+int konami_progress = 0;
+int konami_timer = 60;
+bool konami_triggered = FALSE;
 
-void konamiInput(u16 joy, u16 changed, u16 state){    
-
+void konamiInput(u16 joy, u16 changed, u16 state){
     
-    konamiTimer = 60;
+    konami_timer = 60;
 
-    // U U D D L R L R B A START 
-    // 0 1 2 3 4 5 6 7 8 9 10
-
+    //    U U D D L R L R B A START 
+    //    0 1 2 3 4 5 6 7 8 9 10
 
 
     if (state & BUTTON_UP)
     {
-        if (konamiSeq == 0 | konamiSeq == 1 ) {
-            konamiSeq += 1;            
+        if (konami_progress == 0 | konami_progress == 1 ) {
+            konami_progress += 1;            
         }
-        else konamiSeq = 0;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_DOWN)
     {
-        if (konamiSeq == 2 | konamiSeq == 3 ) {
-            konamiSeq += 1;            
+        if (konami_progress == 2 | konami_progress == 3 ) {
+            konami_progress += 1;            
         }
-        else konamiSeq = 0;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_LEFT)
     {
-        if (konamiSeq == 4 | konamiSeq == 6) {
-            konamiSeq += 1;            
+        if (konami_progress == 4 | konami_progress == 6) {
+            konami_progress += 1;            
         }
-        else konamiSeq = 0;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_RIGHT)
     {
-        if (konamiSeq == 5 | konamiSeq == 7 ) {
-            konamiSeq += 1;            
+        if (konami_progress == 5 | konami_progress == 7 ) {
+            konami_progress += 1;            
         }
-        else konamiSeq = 0;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_A)
     {
-        if (konamiSeq == 9 ) {
-            konamiSeq += 1;            
+        if (konami_progress == 9 ) {
+            konami_progress += 1;            
         }
-        else konamiSeq = 0;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_B)
     {
-        if (konamiSeq == 8 ) konamiSeq += 1;
-        else konamiSeq = 0;
+        if (konami_progress == 8 ) konami_progress += 1;
+        else konami_progress = 0;
     }
     else if (state & BUTTON_START)
     {
-        if (konamiSeq == 10 ) konamiSeq += 1;
-        else konamiSeq = 0;
+        if (konami_progress == 10 ) konami_progress += 1;
+        else konami_progress = 0;
     }
     
 
-    if (konamiSeq > 10 ) konamiTriggered = TRUE;
+    if (konami_progress > 10 ) konami_triggered = TRUE;
     
 
 }
 
 void konamiRoutine(){
-        if (konamiTimer > 0) konamiTimer--;
-        if (konamiTimer == 0) konamiSeq = 0;
+    if (konami_timer > 0) konami_timer--;
+    if (konami_timer == 0) konami_progress = 0;
 }
