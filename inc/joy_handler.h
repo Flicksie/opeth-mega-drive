ac5ad28510d01e7a58c7bc85d4bd05351721214a
Tsumari <lucasflicky@hotmail.com>
2022-05-18 11:38:06 -0300

    add konami code support

--- inc/joy_handler.h
+++ inc/joy_handler.h
@@ -3,5 +3,10 @@
 
 void joyHandlerTitle(u16 joy, u16 changed, u16 state);
 void joyHandlerMenu(u16 joy, u16 changed, u16 state);
+void konami(u16 joy, u16 changed, u16 state);
+
+extern int konamiSeq;
+extern int konamiTimer;
+bool konamiPositive;
 
 #endif
