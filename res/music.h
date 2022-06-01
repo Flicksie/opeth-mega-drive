519669175f4c871d8a1142d1f041052ae3bb0230
Tsumari <lucasflicky@hotmail.com>
2022-06-01 03:43:48 -0300

    new songs

--- res/music.h
+++ res/music.h
@@ -2,6 +2,8 @@
 #define _RES_MUSIC_H_
 
 extern const u8 akersega[54784];
+extern const u8 sfx_cursor[1024];
+extern const u8 sfx_confirm[3584];
 extern const u8 pcm_hat1[9728];
 extern const u8 pcm_hat2[5632];
 extern const u8 pcm_voice[41728];
@@ -9,12 +11,18 @@ extern const u8 pcm_snare1[9472];
 extern const u8 pcm_snare2[3840];
 extern const u8 wpane_mini[2304];
 extern const u8 brose_mini[33536];
+extern const u8 bgm_absent[32512];
+extern const u8 bgm_willo[25856];
 extern const u8 xgm_1[212992];
 extern const u8 xgm_2[150272];
-extern const u8 xgm_3[10496];
+extern const u8 xgm_3[321024];
 extern const u8 xgm_4[346368];
 extern const u8 xgm_5[251392];
 extern const u8 xgm_6[156416];
 extern const u8 xgm_7[168960];
+extern const u8 xgm_8[218624];
+extern const u8 xgm_9[32512];
+extern const u8 xgm_10[27136];
+extern const u8 xgm_11[12032];
 
 #endif // _RES_MUSIC_H_
